48730dd3514709d9bc4a43219ff1143dddc9a5f2
Krzysztof Zablocki <krzysztof.zablocki@pixle.pl>
2016-12-11 19:14:59 +0100

    feat: use Insanity to code-generate Insanity boilerplate

--- Insanity/Models/Variable.swift
+++ Insanity/Models/Variable.swift
@@ -7,8 +7,6 @@ import Foundation
 
 /// Defines a variable
 class Variable: NSObject {
-    private var accessLevel: (read: AccessLevel, write: AccessLevel)
-
     /// Variable name
     var name: String
 
@@ -22,35 +20,17 @@ class Variable: NSObject {
     var isStatic: Bool
 
     /// Read access
-    var readAccess: AccessLevel {
-        return accessLevel.read
-    }
+    var readAccess: AccessLevel
 
     /// Write access
-    var writeAccess: AccessLevel {
-        return accessLevel.write
-    }
+    var writeAccess: AccessLevel
 
     init(name: String, type: String, accessLevel: (read: AccessLevel, write: AccessLevel) = (.internal, .internal), isComputed: Bool = false, isStatic: Bool = false) {
         self.name = name
         self.type = type
-        self.accessLevel = accessLevel
         self.isComputed = isComputed
         self.isStatic = isStatic
-    }
-
-    override func isEqual(_ object: Any?) -> Bool {
-        guard let rhs = object as? Variable else {
-            return false
-        }
-
-        let lhs = self
-        return
-            lhs.type == rhs.type &&
-            lhs.accessLevel.read == rhs.accessLevel.read &&
-            lhs.accessLevel.write == rhs.accessLevel.write &&
-            lhs.name == rhs.name &&
-            lhs.isComputed == rhs.isComputed &&
-            lhs.isStatic == rhs.isStatic
+        self.readAccess = accessLevel.read
+        self.writeAccess = accessLevel.write
     }
 }
