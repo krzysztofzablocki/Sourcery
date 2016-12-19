0131ae48f506d044980aa5d8f23c26aac7723f30
Valentin Knabel <dev@vknabel.com>
2016-12-20 00:05:53 +0100

    Fixes build by using forks

--- Package.swift
+++ Package.swift
@@ -3,9 +3,11 @@ import PackageDescription
 let package = Package(
     name: "Sourcery",
     dependencies: [
-        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/kylef/Commander.git", "0.6.0"),
-        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 7),
+        // https://github.com/kylef/Stencil/pull/84
+        .Package(url: "https://github.com/vknabel/Stencil.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
+        // Requires new release including https://github.com/kylef/PathKit/commit/7b17207
+        .Package(url: "https://github.com/vknabel/PathKit.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 15),
         .Package(url: "https://github.com/vknabel/SwiftTryCatch.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/krzysztofzablocki/KZFileWatchers.git", majorVersion: 1, minor: 0),
