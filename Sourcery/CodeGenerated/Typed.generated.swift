c2af0bb464e02faf054ba5b83b446ebda085a02d
Ilya Puchka <ilyapuchka@gmail.com>
2017-01-06 23:09:13 +0100

    feat: actualTypeName on Typed return typeName for non alias types

--- Sourcery/CodeGenerated/Typed.generated.swift
+++ Sourcery/CodeGenerated/Typed.generated.swift
@@ -5,34 +5,34 @@ extension Enum.Case.AssociatedValue {
     var isOptional: Bool { return typeName.isOptional }
     var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
     var unwrappedTypeName: String { return typeName.unwrappedTypeName }
-    var actualTypeName: TypeName? { return typeName.actualTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
     var isTuple: Bool { return typeName.isTuple }
 }
 extension Method.Parameter {
     var isOptional: Bool { return typeName.isOptional }
     var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
     var unwrappedTypeName: String { return typeName.unwrappedTypeName }
-    var actualTypeName: TypeName? { return typeName.actualTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
     var isTuple: Bool { return typeName.isTuple }
 }
 extension TupleType.Element {
     var isOptional: Bool { return typeName.isOptional }
     var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
     var unwrappedTypeName: String { return typeName.unwrappedTypeName }
-    var actualTypeName: TypeName? { return typeName.actualTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
     var isTuple: Bool { return typeName.isTuple }
 }
 extension Typealias {
     var isOptional: Bool { return typeName.isOptional }
     var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
     var unwrappedTypeName: String { return typeName.unwrappedTypeName }
-    var actualTypeName: TypeName? { return typeName.actualTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
     var isTuple: Bool { return typeName.isTuple }
 }
 extension Variable {
     var isOptional: Bool { return typeName.isOptional }
     var isImplicitlyUnwrappedOptional: Bool { return typeName.isImplicitlyUnwrappedOptional }
     var unwrappedTypeName: String { return typeName.unwrappedTypeName }
-    var actualTypeName: TypeName? { return typeName.actualTypeName }
+    var actualTypeName: TypeName? { return typeName.actualTypeName ?? typeName }
     var isTuple: Bool { return typeName.isTuple }
 }
