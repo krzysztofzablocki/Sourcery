6c04d0680af8ae0fd5cf2d9993f5aa21a9b440d8
Krzysztof Zablocki <krzysztof.zablocki@pixle.pl>
2017-01-23 20:09:23 +0100

    chore: re-enable performance tests

--- SourceryTests/Sourcery+PerformanceSpec.swift
+++ SourceryTests/Sourcery+PerformanceSpec.swift
@@ -1,43 +1,43 @@
-////
-////  Sourcery+PerformanceSpec.swift
-////  Sourcery
-////
-////  Created by Krzysztof Zabłocki on 26/12/2016.
-////  Copyright © 2016 Pixle. All rights reserved.
-////
 //
-//import XCTest
-//import PathKit
+//  Sourcery+PerformanceSpec.swift
+//  Sourcery
 //
-//@testable
-//import Sourcery
+//  Created by Krzysztof Zabłocki on 26/12/2016.
+//  Copyright © 2016 Pixle. All rights reserved.
 //
-//class SourceryPerformanceSpec: XCTestCase {
-//    let outputDir: Path = {
-//        Path.cleanTemporaryDir(name: "SourceryPerformance")
-//    }()
-//
-//    func testParsingPerformanceOnCleanRun() {
-//        let _ = try? Path.cachesDir(sourcePath: Stubs.sourceForPerformance).delete()
-//
-//        self.measure {
-//            let _ = try? Sourcery().processFiles(Stubs.sourceForPerformance,
-//                                                 usingTemplates: Stubs.templateDirectory + Path("Basic.stencil"),
-//                                                 output: self.outputDir,
-//                                                 cacheDisabled: true)
-//        }
-//    }
-//
-//    func testParsingPerformanceOnSubsequentRun() {
-//        let _ = try? Path.cachesDir(sourcePath: Stubs.sourceForPerformance).delete()
-//        let _ = try? Sourcery().processFiles(Stubs.sourceForPerformance,
-//                                             usingTemplates: Stubs.templateDirectory + Path("Basic.stencil"),
-//                                             output: self.outputDir)
-//
-//        self.measure {
-//            let _ = try? Sourcery().processFiles(Stubs.sourceForPerformance,
-//                                                 usingTemplates: Stubs.templateDirectory + Path("Basic.stencil"),
-//                                                 output: self.outputDir)
-//        }
-//    }
-//}
+
+import XCTest
+import PathKit
+
+@testable
+import Sourcery
+
+class SourceryPerformanceSpec: XCTestCase {
+    let outputDir: Path = {
+        Path.cleanTemporaryDir(name: "SourceryPerformance")
+    }()
+
+    func testParsingPerformanceOnCleanRun() {
+        let _ = try? Path.cachesDir(sourcePath: Stubs.sourceForPerformance).delete()
+
+        self.measure {
+            let _ = try? Sourcery().processFiles(Stubs.sourceForPerformance,
+                                                 usingTemplates: Stubs.templateDirectory + Path("Basic.stencil"),
+                                                 output: self.outputDir,
+                                                 cacheDisabled: true)
+        }
+    }
+
+    func testParsingPerformanceOnSubsequentRun() {
+        let _ = try? Path.cachesDir(sourcePath: Stubs.sourceForPerformance).delete()
+        let _ = try? Sourcery().processFiles(Stubs.sourceForPerformance,
+                                             usingTemplates: Stubs.templateDirectory + Path("Basic.stencil"),
+                                             output: self.outputDir)
+
+        self.measure {
+            let _ = try? Sourcery().processFiles(Stubs.sourceForPerformance,
+                                                 usingTemplates: Stubs.templateDirectory + Path("Basic.stencil"),
+                                                 output: self.outputDir)
+        }
+    }
+}
