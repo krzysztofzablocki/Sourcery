205cf31a8c9c060bcb8f676a4afbf84ae5945c2c
Robbert Brandsma <i@robbertbrandsma.nl>
2017-04-19 20:30:10 +0200

    Basic error reporting from EJS templates

--- Sourcery/Generating/Template/JavaScript/JavaScriptTemplate.swift
+++ Sourcery/Generating/Template/JavaScript/JavaScriptTemplate.swift
@@ -20,18 +20,26 @@ final class JavaScriptTemplate: Template {
         let path = Bundle(for: JavaScriptTemplate.self).path(forResource: "ejsbundle", ofType: "js")!
         let ejs = try String(contentsOfFile: path, encoding: .utf8)
 
+        var errorContent = ""
+
         let include: @convention(block) (String) -> String? = { [unowned self] path in
             let path = self.sourcePath.parent() + Path(path)
             let includedTemplate: String? = try? path.read()
             return includedTemplate
         }
 
+        let rethrow: @convention(block) (NSDictionary, String, String, String) -> Void = { error, source, filename, lineno in
+            errorContent += "// error parsing \(filename):\(lineno)\n"
+        }
+
         jsContext.setObject(template, forKeyedSubscript: "template" as NSString)
         jsContext.setObject(context.jsContext, forKeyedSubscript: "templateContext" as NSString)
         jsContext.setObject(include, forKeyedSubscript: "include" as NSString)
+        jsContext.setObject(rethrow, forKeyedSubscript: "rethrow" as NSString)
+        jsContext.setObject(sourcePath.lastComponent, forKeyedSubscript: "templateName" as NSString)
         jsContext.evaluateScript("var window = this; \(ejs)")
         let content = jsContext.objectForKeyedSubscript("content").toString()
-        return content ?? ""
+        return errorContent + (content ?? "")
     }
 
 }
