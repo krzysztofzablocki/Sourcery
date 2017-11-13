724caee14329ec856cbecd951f019d81fc059cb0
Luca Bartoletti <luca@citymapper.com>
2017-11-13 20:57:04 +0000

    Move from deprecated NonNilMatcherFunc to Predicate in CustomMatchers

--- SourceryTests/Helpers/CustomMatchers.swift
+++ SourceryTests/Helpers/CustomMatchers.swift
@@ -13,50 +13,51 @@ import Quick
 /// Values can support equal by supporting the Equatable protocol.
 ///
 /// @see beCloseTo if you want to match imprecise types (eg - floats, doubles).
-public func equal<T: Equatable>(_ expectedValue: T?) -> NonNilMatcherFunc<T> where T: Diffable {
-    return NonNilMatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "equal <\(stringify(expectedValue))>"
+public func equal<T: Equatable>(_ expectedValue: T?) -> Predicate<T> where T: Diffable {
+    return  Predicate.define("equal <\(stringify(expectedValue))>") { actualExpression, msg -> PredicateResult in
         let actualValue = try actualExpression.evaluate()
         let matches = actualValue == expectedValue && expectedValue != nil
         if expectedValue == nil || actualValue == nil {
             if expectedValue == nil {
-                failureMessage.postfixActual = " (use beNil() to match nils)"
+                return  PredicateResult(
+                    status: .fail,
+                    message: msg.appendedBeNilHint()
+                )
             }
-            return false
+            return  PredicateResult(
+                status: .fail,
+                message: msg
+            )
         }
 
-        if !matches, let actual = actualValue, let expected = expectedValue {
-            let results = DiffableResult()
-            results.trackDifference(actual: actual, expected: expected)
-            prepare(failureMessage, results: results, actual: stringify(actualValue))
-        }
-        return matches
+        return PredicateResult(status: PredicateStatus(bool: matches),
+                               message: msg)
     }
 }
 
 /// A Nimble matcher that succeeds when the actual collection is equal to the expected collection.
 /// Items must implement the Equatable protocol.
-public func equal<T: Equatable>(_ expectedValue: [T]?) -> NonNilMatcherFunc<[T]> where T: Diffable {
-    return NonNilMatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "equal <\(stringify(expectedValue))>"
+public func equal<T: Equatable>(_ expectedValue: [T]?) -> Predicate<[T]> where T: Diffable {
+    return  Predicate.define("equal <\(stringify(expectedValue))>") { actualExpression, msg -> PredicateResult in
         let actualValue = try actualExpression.evaluate()
         if expectedValue == nil || actualValue == nil {
             if expectedValue == nil {
-                failureMessage.postfixActual = " (use beNil() to match nils)"
+                return  PredicateResult(
+                    status: .fail,
+                    message: msg.appendedBeNilHint()
+                    )
             }
-            return false
+            return  PredicateResult(
+                status: .fail,
+                message: msg
+            )
         }
 
         // swiftlint:disable:next force_unwrapping
         let matches = expectedValue! == actualValue!
 
-        if !matches, let expected = expectedValue, let actual = actualValue {
-            let results = DiffableResult()
-            results.trackDifference(actual: actual, expected: expected)
-            prepare(failureMessage, results: results, actual: stringify(actual))
-        }
-
-        return matches
+        return PredicateResult(status: PredicateStatus(bool: matches),
+                               message: msg)
     }
 }
 
@@ -64,32 +65,26 @@ public func equal<T: Equatable>(_ expectedValue: [T]?) -> NonNilMatcherFunc<[T]>
 /// Values can support equal by supporting the Equatable protocol.
 ///
 /// @see beCloseTo if you want to match imprecise types (eg - floats, doubles).
-public func equal<T: Equatable, C: Equatable>(_ expectedValue: [T: C]?) -> NonNilMatcherFunc<[T: C]> where C: Diffable {
-    return NonNilMatcherFunc { actualExpression, failureMessage in
-        failureMessage.postfixMessage = "equal <\(stringify(expectedValue))>"
+public func equal<T, C: Equatable>(_ expectedValue: [T: C]?) -> Predicate<[T: C]> where C: Diffable {
+    return Predicate.define("equal <\(stringify(expectedValue))>") { actualExpression, msg -> PredicateResult in
         let actualValue = try actualExpression.evaluate()
         if expectedValue == nil || actualValue == nil {
             if expectedValue == nil {
-                failureMessage.postfixActual = " (use beNil() to match nils)"
+                return  PredicateResult(
+                    status: .fail,
+                    message: msg.appendedBeNilHint()
+                )
             }
-            return false
+            return  PredicateResult(
+                status: .fail,
+                message: msg
+            )
         }
 
         // swiftlint:disable:next force_unwrapping
         let matches = expectedValue! == actualValue!
 
-        if !matches, let expected = expectedValue, let actual = actualValue {
-            let results = DiffableResult()
-            results.trackDifference(actual: actual, expected: expected)
-            prepare(failureMessage, results: results, actual: stringify(actual))
-        }
-
-        return matches
-    }
-}
-
-private func prepare(_ message: FailureMessage, results: DiffableResult, actual: String) {
-    if !results.isEmpty {
-        message.stringValue = "\(results)\n Actual: \(actual)\(message.postfixActual)"
+        return PredicateResult(status: PredicateStatus(bool: matches),
+                               message: msg)
     }
 }
