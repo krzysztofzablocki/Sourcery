88f803ec0ca8ebdaed7d2ea4fc500a715e1899af
Stijn Willems <stijn.willems@dooz.be>
2018-03-21 14:07:08 +0100

    #557 added AutoGenerateProtocol template

---
+++ Templates/Tests/Context/AutoGenerateProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  AutoGenerateProtocol.swift
+//  TemplatesTests
+//
+//  Created by Stijn on 21/03/2018.
+//  Copyright © 2018 Pixle. All rights reserved.
+//
+
+import Foundation
+
+protocol AutoGenerateProtocol {}
+
+struct Car: AutoGenerateProtocol {
+
+    var mutable: String
+    let immutable: String
+
+    // private variables will be ignored
+    private let privateImmutable: String
+
+    init(privateParam: String) {
+        self.mutable = ""
+        self.immutable = "immutable"
+        self.privateImmutable = privateParam
+    }
+
+    // sourcery:includeInitInProtocol
+    init() {
+        self.init(privateParam: "convenience init")
+    }
+
+    func foo() {
+        // nothing
+    }
+
+    // sourcery:skipProtocol
+    func skippedFuntion() {
+
+    }
+}
