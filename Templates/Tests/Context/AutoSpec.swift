d38a3aeb8305bb86692ed96b39d4a9e77b38e59b
Stijn Willems <stijn.willems@dooz.be>
2018-03-22 15:36:11 +0100

    #559 added template to generate specs

---
+++ Templates/Tests/Context/AutoSpec.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  TemplatesTests
+//
+//  Created by Stijn on 22/03/2018.
+//  Copyright © 2018 Pixle. All rights reserved.
+//
+
+import Foundation
+
+protocol AutoSpecable { }
+
+struct BusService: Decodable, AutoSpecable {
+    // sourcery: typeNotMockable
+    // sourcery: testValue = ""Johny safely""
+    // sourcery: customMock = ""Johny safely""
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
---
+++ Templates/Tests/Generated/AutoSpecable.generated.swift
@@ -0,0 +1,50 @@
+// Generated using Sourcery 0.10.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+//: Code only works auto generated for files implementing file tags
+//: This code is not build or run. You can just copy paste it into your test file to have a start for the tests
+
+/*
+This code generates a spec template. Copy paste in to your spec file and rename class by removing the Auto.
+Every test starts with sut (Subject Under Test) and its parameters that will be mocks.
+For variables an methods you can use the following annotations:
+* sourcery: typeNotMockable : will init the mock with the real value and init()
+* sourcery: customMock = Mock() : will replace set mock to Mock() instead of TypeMock()
+* sourcery: testValue : will add after initialization a test with == to this value like expect(sut.variable) == testValue (this also works for adding values to parameters)
+* sourcery: skipSpec : will not add variable or function to spec
+* sourcery: nestedVariableToTest : expect(sut.variable.nestedVariableToTest) == testValue
+* sourcery: expectedReturnValue : expect(result) == expectedReturnValue
+*/
+
+import Quick
+import Nimble
+// add custom import here
+
+@testable import Templates // Replace with the test target you are testing
+
+// MARK: - BusServiceAutoSpec
+
+final class BusServiceAutoSpec: QuickSpec {
+func testSpec() { spec() }
+
+override func spec() {
+describe("BusService") {
+var sut: BusService!
+
+var name: String!
+
+beforeEach {
+name = "Johny safely"
+ sut = BusService(
+name: name
+)
+}
+// MARK: - Test variables
+
+it("sets name") {
+expect(sut.name) == "Johny safely"
+}
+}
+}
+}
