c8553056ce780eccbf269d55d17f8871d73dd836
Kevin Lundberg <kevin@klundberg.com>
2019-01-22 05:54:30 -0500

    Use updated version of SwiftTryCatch (#718)
    * Use released version of SwiftTryCatch
    
    * Use latest commit since 1.0.0 doesn't seem to work

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/jpsim/SourceKitten.git", .exact("0.21.2")),
         .package(url: "https://github.com/kylef/Stencil.git", .exact("0.13.1")),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .exact("2.7.0")),
-        .package(url: "https://github.com/seanparsons/SwiftTryCatch.git", .revision("e7074a72e4d4dc516f391bc4d4afd8ca6a845b4b")),
+        .package(url: "https://github.com/seanparsons/SwiftTryCatch.git", .revision("6a177252cfa2ef649b0aa7b2d16ab221386ca51c")),
         .package(url: "https://github.com/tuist/xcodeproj", .exact("4.3.1")),
         .package(url: "https://github.com/tadija/AEXML.git", .exact("4.3.3")),
     ],
