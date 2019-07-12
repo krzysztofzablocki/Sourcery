1950f59a4bd12ec35ea47f2cb06b46ff835eab4f
Liquidsoul <liquidsoul@liquidsoul.fr>
2019-07-12 18:29:04 +0200

    Remove SPM dependencies not used by any target

--- Package.swift
+++ Package.swift
@@ -18,10 +18,8 @@ let package = Package(
         // PathKit needs to be exact to avoid a SwiftPM bug where dependency resolution takes a very long time.
         .package(url: "https://github.com/kylef/PathKit.git", .exact("0.9.2")),
         .package(url: "https://github.com/jpsim/SourceKitten.git", .exact("0.21.2")),
-        .package(url: "https://github.com/kylef/Stencil.git", .exact("0.13.1")),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .exact("2.7.0")),
         .package(url: "https://github.com/tuist/xcodeproj", .exact("4.3.1")),
-        .package(url: "https://github.com/tadija/AEXML.git", .exact("4.3.3")),
     ],
     targets: [
         .target(name: "Sourcery", dependencies: [
