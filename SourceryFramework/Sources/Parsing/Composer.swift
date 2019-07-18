9e6999ec48a230beff977a588aca9558ab785c08
Sébastien Duperron <liquidsoul@liquidsoul.fr>
2019-07-18 16:20:39 +0200

    Migrate Sourcery code to Swift 5 (#773)
    * Remove SPM dependencies not used by any target
    
    * Set SWIFT_VERSION to 5.0 in the Xcode project
    
    * Fix index(of:) deprecation warnings
    
    * Fix withUnsafeBytes deprecation warning in sha256 function
    
    * Fix English localization deprecation warning

--- SourceryFramework/Sources/Parsing/Composer.swift
+++ SourceryFramework/Sources/Parsing/Composer.swift
@@ -327,7 +327,7 @@ public struct Composer {
                     let valueName = self.actualTypeName(for: TypeName(types[1]), containingType: containingType, unique: unique, typealiases: typealiases)
                     actualTypeName = "[\(keyName ?? types[0]): \(valueName ?? types[1])]"
                 }
-            } else if let genericStartIndex = unwrappedTypeName.index(of: "<"), unwrappedTypeName.last == ">" {
+            } else if let genericStartIndex = unwrappedTypeName.firstIndex(of: "<"), unwrappedTypeName.last == ">" {
                 let genericTypeNameString = String(unwrappedTypeName.prefix(upTo: genericStartIndex))
                 let genericTypeName = self.actualTypeName(for: TypeName(genericTypeNameString), containingType: containingType, unique: unique, typealiases: typealiases)
 
--- SourceryUtils/Sources/Sha.swift
+++ SourceryUtils/Sources/Sha.swift
@@ -9,10 +9,10 @@ import CommonCrypto
 extension Data {
     public func sha256() -> Data {
         var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        self.withUnsafeBytes {
-            _ = CC_SHA256($0, CC_LONG(self.count), &hash)
+        self.withUnsafeBytes { (pointer) -> Void in
+            _ = CC_SHA256(pointer.baseAddress, CC_LONG(pointer.count), &hash)
         }
-        return Data(bytes: hash)
+        return Data(hash)
     }
 }
 
