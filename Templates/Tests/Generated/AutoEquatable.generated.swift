b038a47839911535d1070e51c1e1eaeb22bcd0de
Szymon Mrozek <szymon.mrozek.sm@gmail.com>
2020-01-09 19:34:42 +0100

    Add tests and entry to changelog

--- Templates/Tests/Generated/AutoEquatable.generated.swift
+++ Templates/Tests/Generated/AutoEquatable.generated.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 0.16.0 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 0.17.0 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
 // swiftlint:disable file_length
