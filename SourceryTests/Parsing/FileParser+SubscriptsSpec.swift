f211c6d632c58a933b894d9ad7e2a04d5d83e46d
Denys Telezhkin <strangervir@gmail.com>
2020-03-30 11:52:50 +0300

    Verify subscripts also receive as much of known types as possible.

--- SourceryTests/Parsing/FileParser+SubscriptsSpec.swift
+++ SourceryTests/Parsing/FileParser+SubscriptsSpec.swift
@@ -17,29 +17,31 @@ class FileParserSubscriptsSpec: QuickSpec {
                 }
 
                 it("extracts subscripts properly") {
-                    let subscripts = parse("class Foo { final private subscript(\n_ index: Int, a: String\n) -> Int { get { return 0 } set { do {} } }; public private(set) subscript(b b: Int) -> String { get { return \"\"} } set { } } }").first?.subscripts
+                    let subscripts = parse("protocol Interoo {}\n extension Int: Interoo {}\n class Foo { final private subscript(\n_ index: Int, a: String\n) -> Int { get { return 0 } set { do {} } }; public private(set) subscript(b b: Int) -> String { get { return \"\"} } set { } } }").first?.subscripts
 
-                    expect(subscripts?[0]).to(equal(
-                        Subscript(
-                            parameters: [
-                                MethodParameter(argumentLabel: nil, name: "index", typeName: TypeName("Int"), type: Type(name: "Int")),
-                                MethodParameter(argumentLabel: nil, name: "a", typeName: TypeName("String"), type: Type(name: "String"))
-                            ],
-                            returnTypeName: TypeName("Int"),
-                            accessLevel: (.private, .private),
-                            attributes: [
-                                "final": Attribute(name: "final", description: "final"),
-                                "private": Attribute(name: "private", description: "private")
-                            ],
-                            annotations: [:],
-                            definedInTypeName: TypeName("Foo")
-                        )
-                    ))
+                    let intType = Type(name: "Int", isExtension: true, inheritedTypes: ["Interoo"])
+
+                    let expectedSubscript0 = Subscript(
+                        parameters: [
+                            MethodParameter(argumentLabel: nil, name: "index", typeName: TypeName("Int"), type: intType),
+                            MethodParameter(argumentLabel: nil, name: "a", typeName: TypeName("String"), type: Type(name: "String"))
+                        ],
+                        returnTypeName: TypeName("Int"),
+                        accessLevel: (.private, .private),
+                        attributes: [
+                            "final": Attribute(name: "final", description: "final"),
+                            "private": Attribute(name: "private", description: "private")
+                        ],
+                        annotations: [:],
+                        definedInTypeName: TypeName("Foo")
+                    )
+                    expectedSubscript0.returnType = intType
+                    expect(subscripts?[0]).to(equal(expectedSubscript0))
 
                     expect(subscripts?[1]).to(equal(
                         Subscript(
                             parameters: [
-                                MethodParameter(argumentLabel: "b", name: "b", typeName: TypeName("Int"), type: Type(name: "Int"))
+                                MethodParameter(argumentLabel: "b", name: "b", typeName: TypeName("Int"), type: intType)
                             ],
                             returnTypeName: TypeName("String"),
                             accessLevel: (.public, .private),
