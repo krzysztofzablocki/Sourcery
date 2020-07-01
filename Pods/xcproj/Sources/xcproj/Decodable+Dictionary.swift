5ba88053a3043709f2d26c27393f24b0bc4489f3
Roman Podymov <podymfrombryansk@yandex.ru>
2020-07-02 00:24:46 +0200

    public Decodable.init(jsonDictionary:), String -> AnyHashable

--- Pods/xcproj/Sources/xcproj/Decodable+Dictionary.swift
+++ Pods/xcproj/Sources/xcproj/Decodable+Dictionary.swift
@@ -2,13 +2,13 @@ import Foundation
 
 // MARK: - Decodable Extension
 
-extension Decodable {
+public extension Decodable {
 
     /// Initialies the Decodable object with a JSON dictionary.
     ///
     /// - Parameter jsonDictionary: json dictionary.
     /// - Throws: throws an error if the initialization fails.
-    init(jsonDictionary: [String: Any]) throws {
+    init(jsonDictionary: [AnyHashable: Any]) throws {
         let decoder = JSONDecoder()
         let data = try JSONSerialization.data(withJSONObject: jsonDictionary, options: [])
         self = try decoder.decode(Self.self, from: data)
