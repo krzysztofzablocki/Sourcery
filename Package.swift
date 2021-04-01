87b2414abc54b9911e4404d92c3599688e46fb95
HeMet <hemet.mail@gmail.com>
2021-04-01 23:20:57 +0400

    Point Quick to 3.0.0 version

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         .package(name: "SwiftSyntax",
                  url: "https://github.com/apple/swift-syntax.git",
                  .exact("0.50300.0")),
-        .package(url: "https://github.com/Quick/Quick.git", from: "3.1.2"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
         .package(url: "https://github.com/HeMet/Nimble.git", from: "9.0.0")
     ],
     targets: [
