9529244965affeca679a18b1578ff154e3566591
Pol Piella Abadia <hi@polpiella.dev>
2022-09-09 11:14:38 +0100

    Updates dependencies formats

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
-        .package(url: "https://github.com/kylef/Commander.git", .exact("0.9.1")),
+        .package(url: "https://github.com/kylef/Commander.git", exact: "0.9.1"),
         // PathKit needs to be exact to avoid a SwiftPM bug where dependency resolution takes a very long time.
-        .package(url: "https://github.com/kylef/PathKit.git", .exact("1.0.1")),
+        .package(url: "https://github.com/kylef/PathKit.git", exact: "1.0.1"),
         .package(url: "https://github.com/StencilProject/Stencil.git", .upToNextMajor(from: "0.14.0")),
-        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .exact("2.10.1")),
-        .package(url: "https://github.com/tuist/XcodeProj.git", .exact("8.3.1")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50600.1")),
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", exact: "2.10.1"),
+        .package(url: "https://github.com/tuist/XcodeProj.git", exact: "8.3.1"),
+        .package(url: "https://github.com/apple/swift-syntax.git", exact: "0.50600.1"),
         .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0")
     ],
@@ -242,7 +242,8 @@ let package = Package(
                 permissions: [
                     .writeToPackageDirectory(reason: "Need permission to write generated files to package directory")
                 ]
-            )
+            ),
+            dependencies: ["SourceryExecutable"]
         )
     ]
 )
