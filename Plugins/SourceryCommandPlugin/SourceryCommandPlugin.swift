22f9d02d0c8d9861c262bd69ec11940e873c5142
Pol Piella Abadia <hi@polpiella.dev>
2022-09-09 14:46:30 +0100

    Updates command plugin with `Xcode` project support

--- Plugins/SourceryCommandPlugin/SourceryCommandPlugin.swift
+++ Plugins/SourceryCommandPlugin/SourceryCommandPlugin.swift
@@ -9,11 +9,10 @@ struct SourceryCommandPlugin: CommandPlugin {
             let configFilePath = target.directory.appending(subpath: ".sourcery.yml").string
             
             guard FileManager.default.fileExists(atPath: configFilePath) else {
-                Diagnostics.warning("🤷‍♂️ Could not find config at: \(configFilePath), skipping...")
+                Diagnostics.warning("⚠️ Could not find `.sourcery.yml` for the given target")
                 return
             }
             
-            print(configFilePath)
             
             let sourceryExecutable = try context.tool(named: "SourceryExecutable")
             let sourceryURL = URL(fileURLWithPath: sourceryExecutable.path.string)
@@ -36,3 +35,43 @@ struct SourceryCommandPlugin: CommandPlugin {
         }
     }
 }
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SourceryCommandPlugin: XcodeCommandPlugin {
+    func performCommand(context: XcodePluginContext, arguments: [String]) throws {
+        for target in context.xcodeProject.targets {
+            guard let configFilePath = target
+                .inputFiles
+                .filter({ $0.path.lastComponent == ".sourcery.yml" })
+                .first?
+                .path
+                .string else {
+                Diagnostics.warning("⚠️ Could not find `.sourcery.yml` in Xcode's input file list")
+                return
+            }
+
+            let sourceryExecutable = try context.tool(named: "SourceryExecutable")
+            let sourceryURL = URL(fileURLWithPath: sourceryExecutable.path.string)
+            
+            let process = Process()
+            process.executableURL = sourceryURL
+            process.arguments = [
+                "--config",
+                configFilePath,
+                "--disableCache"
+            ]
+            
+            try process.run()
+            process.waitUntilExit()
+            
+            let gracefulExit = process.terminationReason == .exit && process.terminationStatus == 0
+            if !gracefulExit {
+                Diagnostics.error("🛑 The plugin execution failed")
+            }
+        }
+        debugPrint(context)
+    }
+}
+#endif
