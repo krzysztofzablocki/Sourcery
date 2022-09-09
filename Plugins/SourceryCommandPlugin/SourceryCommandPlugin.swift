d328372cc4da84a69ab138034489a36d09ad6206
Pol Piella Abadia <hi@polpiella.dev>
2022-09-09 12:16:52 +0100

    Makes command plugin more `focused` to the target itself and not the package

--- Plugins/SourceryCommandPlugin/SourceryCommandPlugin.swift
+++ Plugins/SourceryCommandPlugin/SourceryCommandPlugin.swift
@@ -4,27 +4,35 @@ import Foundation
 @main
 struct SourceryCommandPlugin: CommandPlugin {
     func performCommand(context: PluginContext, arguments: [String]) async throws {
-        let configFilePath = context.package.directory.appending(subpath: ".sourcery.yml").string
-        guard FileManager.default.fileExists(atPath: configFilePath) else {
-            Diagnostics.error("🤷‍♂️ Could not find config at: \(configFilePath)")
-            return
-        }
-        
-        let sourceryExecutable = try context.tool(named: "SourceryExecutable")
-        let sourceryURL = URL(fileURLWithPath: sourceryExecutable.path.string)
-        
-        let process = Process()
-        process.executableURL = sourceryURL
-        process.arguments = [
-            "--disableCache"
-        ]
-        
-        try process.run()
-        process.waitUntilExit()
-        
-        let gracefulExit = process.terminationReason == .exit && process.terminationStatus == 0
-        if !gracefulExit {
-            Diagnostics.error("🛑 The plugin execution failed")
+        // Run one per target
+        for target in context.package.targets {
+            let configFilePath = target.directory.appending(subpath: ".sourcery.yml").string
+            
+            guard FileManager.default.fileExists(atPath: configFilePath) else {
+                Diagnostics.warning("🤷‍♂️ Could not find config at: \(configFilePath), skipping...")
+                return
+            }
+            
+            print(configFilePath)
+            
+            let sourceryExecutable = try context.tool(named: "SourceryExecutable")
+            let sourceryURL = URL(fileURLWithPath: sourceryExecutable.path.string)
+            
+            let process = Process()
+            process.executableURL = sourceryURL
+            process.arguments = [
+                "--config",
+                configFilePath,
+                "--disableCache"
+            ]
+            
+            try process.run()
+            process.waitUntilExit()
+            
+            let gracefulExit = process.terminationReason == .exit && process.terminationStatus == 0
+            if !gracefulExit {
+                Diagnostics.error("🛑 The plugin execution failed")
+            }
         }
     }
 }
