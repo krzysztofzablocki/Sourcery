3a9e5f1014ac09b30727c01d2a0e1531dac30948
Krzysztof Zablocki <krzysztof.zablocki@pixle.pl>
2022-12-07 12:49:29 -0500

    update internal boilerplate code.

--- SourceryRuntime/Sources/Generated/Diffable.generated.swift
+++ SourceryRuntime/Sources/Generated/Diffable.generated.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 1.9.1 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 1.9.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 import Foundation
 
