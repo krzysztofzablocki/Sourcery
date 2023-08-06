5e280adb64e7dfd5f2d3e33ed461f557b32b600d
Ruslan Alikhamov <r.alikhamov@gmail.com>
2023-08-06 06:03:58 -0400

    enabled sha256 for macOS

--- SourceryUtils/Sources/Sha.swift
+++ SourceryUtils/Sources/Sha.swift
@@ -4,16 +4,21 @@
 //
 
 import Foundation
-//import CommonCrypto
+#if os(macOS)
+import CommonCrypto
+#endif
 
 extension Data {
     public func sha256() -> Data {
+        #if os(macOS)
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        self.withUnsafeBytes { (pointer) -> Void in
+            _ = CC_SHA256(pointer.baseAddress, CC_LONG(pointer.count), &hash)
+        }
+        return Data(hash)
+        #else
         self
-        // var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        // self.withUnsafeBytes { (pointer) -> Void in
-        //     _ = CC_SHA256(pointer.baseAddress, CC_LONG(pointer.count), &hash)
-        // }
-        // return Data(hash)
+        #endif
     }
 }
 
