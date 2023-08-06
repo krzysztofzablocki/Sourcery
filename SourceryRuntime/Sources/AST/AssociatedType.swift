9ccf240cb6514d77c2cbee48b18871a95f8a32d1
Ruslan Alikhamov <r.alikhamov@gmail.com>
2023-08-06 05:05:53 -0400

    enabled fatalError instead of NSException

--- SourceryRuntime/Sources/AST/AssociatedType.swift
+++ SourceryRuntime/Sources/AST/AssociatedType.swift
@@ -3,11 +3,11 @@ import Foundation
 #if !os(macOS)
 public class NSException {
     static func raise(_ name: String, format: String, arguments: CVaListPointer) {
-
+        fatalError ("\(name) exception: \(NSString(format: format, arguments: arguments))")
     }
 
     static func raise(_ name: String) {
-
+        fatalError("\(name) exception")
     }
 }
 #endif
