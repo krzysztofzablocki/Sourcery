1513e4f61a369412f7d6478845d7c3f1f31b7063
David Jennes <david.jennes@gmail.com>
2023-10-30 03:44:10 +0100

    Tests: rename foo & bar types

--- SourceryTests/Stub/Source/Foo.swift
+++ SourceryTests/Stub/Source/Foo.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class Foo {
+class FooBarBaz {
 	// Note: when Swift Linux doesn't bug out on [String: String], add a test back for it
 	// See https://github.com/krzysztofzablocki/Sourcery/pull/1208#issuecomment-1752185381
     typealias Name = String
@@ -11,10 +11,10 @@ class Foo {
 
 protocol AutoEquatable {}
 
-class FooSubclass: Foo, AutoEquatable {
+class FooSubclass: FooBarBaz, AutoEquatable {
     var other: String = ""
 }
 
-func performFoo(value: Foo) {
+func performFoo(value: FooBarBaz) {
 
 }
