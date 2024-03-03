078369214d23d75427f1c62e6c590fc12a4e1636
Ruslan Alikhamov <r.alikhamov@gmail.com>
2024-03-03 18:20:00 +0400

    Implement GenericRequirement support for member type disambiguation (#1283)
    * Implement GenericRequirement support for member type disambiguation
    
    * Reverted redundant change
    
    * Added missing argument for Type_Linux
    
    * Updated generated files
    
    * Moved Type+SwiftSyntax to separate file
    
    * Removed redundant protocol conformance

--- SourceryFramework/Sources/Parsing/SwiftSyntax/AST/Class+SwiftSyntax.swift
+++ SourceryFramework/Sources/Parsing/SwiftSyntax/AST/Class+SwiftSyntax.swift
@@ -6,6 +6,15 @@ extension Class {
     convenience init(_ node: ClassDeclSyntax, parent: Type?, annotationsParser: AnnotationsParser) {
         let modifiers = node.modifiers?.map(Modifier.init) ?? []
 
+        let genericRequirements: [GenericRequirement] = node.genericWhereClause?.requirementList.compactMap { requirement in
+            if let sameType = requirement.body.as(SameTypeRequirementSyntax.self) {
+                return GenericRequirement(sameType)
+            } else if let conformanceType = requirement.body.as(ConformanceRequirementSyntax.self) {
+                return GenericRequirement(conformanceType)
+            }
+            return nil
+        } ?? []
+
         self.init(
           name: node.identifier.text.trimmingCharacters(in: .whitespaces),
           parent: parent,
@@ -17,6 +26,7 @@ extension Class {
           inheritedTypes: node.inheritanceClause?.inheritedTypeCollection.map { $0.typeName.description.trimmed } ?? [],
           containedTypes: [],
           typealiases: [],
+          genericRequirements: genericRequirements,
           attributes: Attribute.from(node.attributes),
           modifiers: modifiers.map(SourceryModifier.init),
           annotations: annotationsParser.annotations(from: node),
