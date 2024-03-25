9a8c14e4306242d33d1fe676facad35a88d0519a
David Cacenabes <david.cacenabes@glovoapp.com>
2024-03-25 14:58:09 +0100

    Remove from generated files

--- SourceryTests/Models/TypedSpec.generated.swift
+++ SourceryTests/Models/TypedSpec.generated.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 2.1.8 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 import Quick
 import Nimble
--- Templates/Tests/Generated/LinuxMain.generated.swift
+++ Templates/Tests/Generated/LinuxMain.generated.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 1.3.0 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 import XCTest
 
