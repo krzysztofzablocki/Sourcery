18fac39880b8a8e72a9a2d8344e718e05f9ad556
Ruslan Alikhamov <r.alikhamov@gmail.com>
2024-03-29 20:53:34 +0400

    docs: update metadata for 2.2.0 release

--- SourceryUtils/Sources/Version.swift
+++ SourceryUtils/Sources/Version.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public struct SourceryVersion {
     public let value: String
-    public static let current = SourceryVersion(value: inUnitTests ? "Major.Minor.Patch" : "2.1.8")
+    public static let current = SourceryVersion(value: inUnitTests ? "Major.Minor.Patch" : "2.2.0")
 }
 
 #if canImport(ObjectiveC)
