f0970d3da40844099736e61e4f2cf0df7286516e
Eric Horacek <eric.horacek@openai.com>
2025-11-08 14:58:47 -0800

    Break out of typealias cycles
    When there’s a typealias cycle, we currently loop infinitely and eventually consume all system memory and crash.

--- SourceryRuntime/Sources/Common/Composer/ParserResultsComposed.swift
+++ SourceryRuntime/Sources/Common/Composer/ParserResultsComposed.swift
@@ -237,8 +237,14 @@ internal struct ParserResultsComposed {
         typealiasesByNames.forEach { _, alias in
             var aliasNamesToReplace = [alias.name]
             var finalAlias = alias
+            var visitedAliasNames = Set(aliasNamesToReplace)
             while let targetAlias = typealiasesByNames[finalAlias.typeName.name] {
+                if visitedAliasNames.contains(targetAlias.name) {
+                    Log.astWarning("Detected typealias cycle while resolving '\(alias.name)' -> '\(targetAlias.name)' (type name: \(finalAlias.typeName.name)).")
+                    break
+                }
                 aliasNamesToReplace.append(targetAlias.name)
+                visitedAliasNames.insert(targetAlias.name)
                 finalAlias = targetAlias
             }
 
